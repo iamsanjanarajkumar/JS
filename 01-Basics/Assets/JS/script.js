2b64b7a651bae224212b89cb62b2082f3726ab55
iamsanjanarajkumar <iamsanjanarajkumar@gmail.com>
2023-06-22 15:19:06 +0530

    first commit of JS

---
+++ 01-Basics/Assets/JS/script.js
@@ -0,0 +1,70 @@
+var a = 12;
+const b = 33;
+let c = 44;
+var d = 50;
+
+document.write(`<p>a= ${a}</p>`);
+document.write(`<p>b= ${b}</p>`);
+document.write(`<p>c= ${c}</p>`);
+document.write(`<p>d= ${d}</p><hr>`);
+
+if (d > 20) {
+  //block
+  var x = 100;
+  let y = 200;
+  const z = 300;
+
+  document.write(`<p>x= ${x}</p>`);
+  document.write(`<p>y= ${y}</p>`);
+  document.write(`<p>z= ${z}</p><hr>`);
+}
+
+document.write(`<p>x= ${x}</p>`);
+// document.write(`<p>y= ${y}</p>`);
+// document.write(`<p>z= ${z}</p><hr>`);
+
+var isAdmin = false;
+document.write(`<p>isAdmin= ${typeof isAdmin}</p>`);
+document.write(`<p>isAdmin= ${isAdmin}</p>`);
+
+var title =  "welcome to js";
+  document.write(`<p>title = ${typeof(title)}</p>`);
+  document.write(`<p>title = ${title}</p>`);
+
+  var xx = 12342;
+  document.write(`<p>xx= ${typeof(xx)}</p>`);
+  document.write(`<p>isAdmin = ${xx}</p>`);
+
+  var yy = 34.343; /* number,float,hexa and exponential - > number */
+  document.write(`<p>yy = ${typeof(yy)}</p>`);
+  document.write(`<p>yy = ${yy}</p>`);
+   
+  //Array
+  var colors = ["orange", "blue", "red", "green", 100, true, 500.5];
+  document.write(`<p>colors = ${typeof(colors)}</p>`);
+  document.write(`<p>colors = ${colors}</p>`);
+   
+  //object
+
+  var user = {
+      //key : value, => property
+      name: "Ramesh",
+      email: "sanjanaraj1672@gmail.com"
+  };
+  document.write(`<p>user = ${typeof(user)}</p>`);
+  document.write(`<p>user = ${user}</p>`);
+   
+  //function block = behavior
+  //function name (params)
+  function defaultFun()// Function Declaration
+  {
+      document.write(`<p>This is DEFAULT FUNCTION.</p>`);
+  }
+  defaultFun(); // Function calling
+
+  function parameterFun(name,email){
+      document.write(`<p>This is PARAMETRIZED FUNCTION.</p>`);
+      document.write(`<p>First parameter is : ${name}</p>`);
+      document.write(`<p>Second parameter is : ${email}</p>`);
+  }
+  parameterFun("JavaScript","admin@javascript.com")
